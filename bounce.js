68bf44364a082856685f7a5795961ee1ff4573b4
giladyavneh <giladov@gmail.com>
2020-07-28 23:56:08 +0300

    created the ball object

--- bounce.js
+++ bounce.js
@@ -1,1 +1,15 @@
-bounce.js
+function Ball(x,y,r,color){
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    this.m=r;
+    this.color=color
+}
+Ball.prototype.draw(elem){
+    let ball=document.createElement("circle")
+    ball.cx=this.x
+    ball.cy=this.y
+    ball.r=this.r
+    ball.fill=this.color
+    elem.append(ball)
+}
