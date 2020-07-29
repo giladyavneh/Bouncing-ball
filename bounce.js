cb2c2d0ac5175fcc12efdb56f35332fbe00d5660
giladyavneh <giladov@gmail.com>
2020-07-29 13:09:15 +0300

    got gravity going as per #2

--- bounce.js
+++ bounce.js
@@ -1,9 +1,12 @@
+//Ball
 function Ball(x,y,r,color){
     this.x=x;
     this.y=y;
     this.r=r;
     this.m=r;
     this.color=color
+    this.v={x:0,y:0}
+    this.acc={x:0,y:0}
 }
 Ball.prototype.draw=function(elem){
     let ctx=elem.getContext("2d");
@@ -13,10 +16,27 @@ Ball.prototype.draw=function(elem){
     ctx.fillStyle="white";
     ctx.fill()
 }
+Ball.prototype.addVector=function(base,{x,y}){
+    base.x+=x
+    base.y+=y
+}
+Ball.prototype.move=function(){
+    this.addVector(this.v,this.acc)
+    this.addVector(this,this.v)
+}
+
+//Forces
+function gravity(obj){
+    obj.addVector(obj.acc,{x:0,y:0.7})
+}
+
+//main
 let ball=new Ball(500,150,50,"white")
 let canvas=document.querySelector("#canvas")
 document.onload=update()
 function update(){
+    gravity(ball)
+    ball.move()
     ball.draw(canvas)
-    requestAnimationFrame(update)
+    let stopID=requestAnimationFrame(update)
 }
